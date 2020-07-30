96563b33de029a5a401e94f2aad25d59006f66dc
Marmex Mander <vadim.m.lutsk@gmail.com>
2020-07-30 19:50:45 +0300

    Add Field interface

--- Game/Interfaces.cs
+++ Game/Interfaces.cs
@@ -7,12 +7,31 @@ using System.Windows.Controls;
 
 namespace Game
 {
-    interface Fish
+    public interface IObserver
+    {
+        void Update(IObservable subject);
+    }
+    public interface IObservable
+    {
+        void Attach(IObserver observer);
+
+        void Detach(IObserver observer);
+
+        void Notify();
+    }
+    interface IFish : IObservable
     {
         int Size { get; }
         int Count { get; set; }
         int MaxCount { get; }
         void Die();
         Image Sprite { get; }
+    }
+     interface IField : IObserver
+    {
+        IFish pullColumn(int column);
+        void pudhColumn(int column, IFish fish);
+        List<List<IFish>> Field { get; }
+
     }
 }
