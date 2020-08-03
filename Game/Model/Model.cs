639c25f860e32426f6b09bf46cc916a02adb93b0
Alex1504n82oa <66085937+Alex1504n82oa@users.noreply.github.com>
2020-08-03 20:43:12 +0300

    Chenges іn  Model

--- Game/Model/Model.cs
+++ Game/Model/Model.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
-namespace Game.Model
+ 
+
+namespace Game
 {
     class Model
     {
+        class TestSmallFish : IFish
+        {
+            public int Size { get; private set; }
+            public int MaxCount { get; private set; }
+            public int Count { get; set; }
+            public Image Sprite { get; private set; }
+            TestSmallFish()
+            {
+                this.Sprite.Source = Controller.BitmapToImageSource(Properties.Resources.SmallFish);
+                Size = 1;
+                MaxCount = 2;
+                Count = 0;
+            }
+            public void Die()
+            {
+                Console.WriteLine("I don't DIE!!");
+
+ 
+
+            }
+            public void Attach(IObserver observer)
+            {
+
+ 
+
+            }
+            public void Detach(IObserver observer)
+            {
+                 
+            }
+            public void Notify()
+            {
+
+ 
+
+            }
+        }
+        
     }
 }
