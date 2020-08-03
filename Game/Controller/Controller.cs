4a1eb92423a1c5e8a46c68844704115004e8bf24
Alex1504n82oa <66085937+Alex1504n82oa@users.noreply.github.com>
2020-08-03 21:17:57 +0300

    TestSmallFish

--- Game/Controller/Controller.cs
+++ Game/Controller/Controller.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
 
-namespace Game.Controller
+namespace Game
 {
     class Controller
     {
