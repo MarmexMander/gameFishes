2f21f543a4f2cf46093b7facc4755a5a9d87bbdf
Alex1504n82oa <66085937+Alex1504n82oa@users.noreply.github.com>
2020-08-04 19:11:41 +0300

    Changes

--- Game/Model/Model.cs
+++ Game/Model/Model.cs
@@ -3,131 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls; 
+using System.Windows.Controls;
+using System.IO;
+ 
 
 namespace Game
 {
     class Model
     {
-        class SmallFish : IFish
+        class TestSmallFish : IFish
         {
-            private List<IObserver> _observers = new List<IObserver>();
             public int Size { get; private set; }
             public int MaxCount { get; private set; }
             public int Count { get; set; }
             public Image Sprite { get; private set; }
-            SmallFish()
+            TestSmallFish()
             {
                 this.Sprite.Source = Controller.BitmapToImageSource(Properties.Resources.SmallFish);
                 Size = 1;
                 MaxCount = 2;
                 Count = 0;
             }
-            public void Die()
-            {
-                Console.WriteLine("I don't DIE!!"); 
 
-            }
-
-            public void Attach(IObserver observer)
-            {
-                this._observers.Add(observer);
-            }
-
-            public void Detach(IObserver observer)
-            {
-                this._observers.Remove(observer);
-            }
-
-            public void Notify()
-            {
-                foreach (var observer in _observers)
-                {
-                    observer.Update(this);
-                }
-            }
-        }
-
-        class MiddleFish : IFish
-        {
-            private List<IObserver> _observers = new List<IObserver>();
-            public int Size { get; private set; }
-            public int MaxCount { get; private set; }
-            public int Count { get; set; }
-            public Image Sprite { get; private set; }
-            MiddleFish()
-            {
-                this.Sprite.Source = Controller.BitmapToImageSource(Properties.Resources.MiddleFish);
-                Size = 2;
-                MaxCount = 2;
-                Count = 0;
-            }
             public void Die()
             {
                 Console.WriteLine("I don't DIE!!");
 
-            }
-
-            public void Attach(IObserver observer)
-            {
-                this._observers.Add(observer);
-            }
-
-            public void Detach(IObserver observer)
-            {
-                this._observers.Remove(observer);
-            }
-
-            public void Notify()
-            {
-                foreach (var observer in _observers)
-                {
-                    observer.Update(this);
-                }
-            }
-        }
-
-        class BigFish : IFish
-        {
-            private List<IObserver> _observers = new List<IObserver>();
-            public int Size { get; private set; }
-            public int MaxCount { get; private set; }
-            public int Count { get; set; }
-            public Image Sprite { get; private set; }
-            BigFish()
-            {
-                this.Sprite.Source = Controller.BitmapToImageSource(Properties.Resources.BigFish);
-                Size = 3;
-                MaxCount = 2;
-                Count = 0;
-            }
-            public void Die()
-            {
-                Console.WriteLine("I don't DIE!!");
+ 
 
             }
 
             public void Attach(IObserver observer)
             {
-                this._observers.Add(observer);
+
+ 
+
             }
 
             public void Detach(IObserver observer)
             {
-                this._observers.Remove(observer);
+                 
             }
 
             public void Notify()
             {
-                foreach (var observer in _observers)
-                {
-                    observer.Update(this);
-                }
+
+ 
+
             }
         }
+        
     }
-
-    
-
-
 }
